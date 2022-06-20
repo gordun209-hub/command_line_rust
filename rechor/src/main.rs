b2e9c7a2050cc43f8705f8a8b0e6c32421a79f7a
gordun209 <ali-han80@hotmail.com>
2022-06-20 19:25:24 +0300

    final

---
+++ rechor/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Ken Youens-Clark <kyclark@gmail.com>")
+        .about("Rust echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = match matches.is_present("omit_newline") {
+        true => " ",
+        false => "\n"
+    };
+    print!("{}{}", text.join(" "), omit_newline);
+}
+
